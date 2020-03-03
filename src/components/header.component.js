0e9058771f303ff7deff138b6806a3a3d6ed22be
eugene <imsorler@gmail.com>
2020-03-04 01:16:34 +0300

    Приветственное окно поместил в local storage

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -6,11 +6,17 @@ export class HeaderComponent extends Component {
     }
 
     init() {
+        //по дефолту скрываем приветсвие
+        if (localStorage.getItem('visited')) {
+            this.hide()
+        }
         const btn = this.$el.querySelector('.js-header-start')
         btn.addEventListener('click', buttonHandler.bind(this))
     }
 }
 
 function buttonHandler() {
+    //с помощью 'visited' убираю отображения приветсвия при обновлении страницы
+    localStorage.setItem('visited', JSON.stringify(true))
     this.hide()
 }
