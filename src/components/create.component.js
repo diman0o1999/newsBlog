6c113504453fd3ed50dc3e52f35b548dcf14a2ba
eugene <imsorler@gmail.com>
2020-03-04 19:29:02 +0300

    Связана база данных Realtime firebase и приложение, в тестовом режиме

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,6 +1,7 @@
 import { Component } from '../core/component'
 import { Form } from '../core/form'
 import { Validators } from '../core/validators'
+import { apiService } from '../services/api.service'
 
 export class CreateComponent extends Component{
     constructor(id) {
@@ -18,19 +19,20 @@ export class CreateComponent extends Component{
     }
 }
 
-function submitHandler(event) {
+async function submitHandler(event) {
     //отключаю перезагрузку старницы
     event.preventDefault()
 
     if (this.form.isValid()) {
         const formData = {
             type: this.$el.type.value,
+            date: new Date().toLocaleDateString(),
             //конвертирую в одно значение
             ...this.form.value()
         }
 
+        await apiService.createPost(formData)
         this.form.clear()
-
-        console.log('submit', formData)
+        alert('Запись создана в базе данных!')
     }
 }
