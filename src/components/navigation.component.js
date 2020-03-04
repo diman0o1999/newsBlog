f480a1aa41772ff20727c1487e6637731ff9a810
eugene <imsorler@gmail.com>
2020-03-04 03:20:24 +0300

    Работает логика в динамическом режиме по показу табов

--- src/components/navigation.component.js
+++ src/components/navigation.component.js
@@ -3,10 +3,17 @@ import { Component } from '../core/component'
 export class NavigationComponent extends Component {
     constructor(id) {
         super(id)
+
+        //получаю доступ до компонентов create, favorite, posts
+        this.tabs = []
     }
 
     init() {
         this.$el.addEventListener('click', tabClickHandler.bind(this))
+    }
+
+    registerTabs(tabs) {
+        this.tabs = tabs
     }
 }
 
@@ -18,7 +25,11 @@ function tabClickHandler(event) {
         Array.from(this.$el.querySelectorAll('.tab')).forEach(tab => {
             tab.classList.remove('active')
         })
-        //добавляю class active
+        //добавляю class active(визуализирую нужный таб)
         event.target.classList.add('active')
+        //                                               в данной переменной храняться названия
+        const activeTab = this.tabs.find(t => t.name === event.target.dataset.name)
+        this.tabs.forEach(t => t.component.hide())
+        activeTab.component.show()
     }
 }
